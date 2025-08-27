6d7f4cedbbb1436c11bba6c1fa9b66adfa741546
Rafabel Santana Villalona <Rafabel1406@gmail.com>
2025-08-27 14:15:34 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Logo className="h

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,9 +3,9 @@ import type { SVGProps } from 'react';
 export function Logo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-        viewBox="0 0 40 40"
-        xmlns="http://www.w3.org/2000/svg"
-        {...props}
+      viewBox="0 0 40 40"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
     >
       <defs>
         <linearGradient id="logo-gradient" x1="0%" y1="0%" x2="100%" y2="100%">
@@ -13,19 +13,11 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
           <stop offset="100%" stopColor="#009CA6" />
         </linearGradient>
       </defs>
-      
-      <circle cx="20" cy="20" r="18" fill="url(#logo-gradient)" />
-      
+      <circle cx="20" cy="20" r="20" fill="white"/>
+      <circle cx="20" cy="20" r="18" fill="none" stroke="url(#logo-gradient)" strokeWidth="2" />
       <path 
         d="M20 12 C 25 17, 26 23, 20 28 C 14 23, 15 17, 20 12 Z"
-        fill="#8CC63F"
-      />
-       <path
-        d="M20.5 12.5 C 20.5 19.5, 26 22, 20.5 28"
-        fill="none"
-        stroke="white"
-        strokeWidth="0.7"
-        strokeLinecap="round"
+        fill="url(#logo-gradient)"
       />
     </svg>
   );
