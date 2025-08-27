7f603c0e09a385a964dadfc7f981a72e40d8aaa8
Rafabel Santana Villalona <Rafabel1406@gmail.com>
2025-08-27 18:11:02 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Footer>
    
    with the

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ import { Button } from "./ui/button";
 
 export function Footer() {
   return (
-    <footer className="bg-card dark:bg-card/50 border-t">
+    <footer className="bg-card dark:bg-card/50 border-t shadow-lg transition-shadow">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div className="md:col-span-1">
