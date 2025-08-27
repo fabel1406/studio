70fe0394cd4f919f39f24026075506a42df19bf2
Rafabel Santana Villalona <Rafabel1406@gmail.com>
2025-08-27 18:25:01 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/offer-dialog.tsx
+++ src/components/offer-dialog.tsx
@@ -4,6 +4,7 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
+import Link from "next/link";
 import {
   Dialog,
   DialogContent,
