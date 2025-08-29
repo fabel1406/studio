a0e8b0c3f0b0eca71331a06df0e077f386ed4ebb
Rafabel Santana Villalona <Rafabel1406@gmail.com>
2025-08-29 03:48:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
