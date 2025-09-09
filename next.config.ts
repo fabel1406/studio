f81dbb690bf68d7a08541a25b1217c7c871af5aa
Rafabel Santana Villalona <Rafabel1406@gmail.com>
2025-09-09 04:14:29 +0000

    Pedi ayuda otra vez para este nuevo error: [00:10:27.820] Running build

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,6 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     // This is to prevent the Edge Runtime warning from Supabase
-    allowMiddlewareResponseBody: true, 
   },
   webpack: (config) => {
     config.ignoreWarnings = [/require\.extensions is not supported by webpack/];
