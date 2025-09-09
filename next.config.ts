179ffcbd387964ab79f0277a7e007f4f33166574
Rafabel Santana Villalona <Rafabel1406@gmail.com>
2025-09-09 00:21:22 +0000

    Ya tienes la Api key, recurda que la ocultamos por seguridad, si quieres

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   webpack: (config) => {
     config.ignoreWarnings = [/require\.extensions is not supported by webpack/];
     return config;
+  },
+  env: {
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
   }
 };
 
